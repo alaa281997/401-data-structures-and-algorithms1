ef811345f092debb0f6126e8d5a2a210477aa211
alaa281997 <alaabbas97@gmail.com>
2021-06-28 23:30:08 +0300

    add c2

---
+++ array-insert-shift/README.md
@@ -0,0 +1,12 @@
+# Code challenge 2
+
+# Insert to Middle of an Array
+ we need  a function called insertShiftArray which takes in an array and a value to be added in middle.
+
+
+## Whiteboard Process
+![alt text](c2.png)
+
+
+## Approach & Efficiency
+I used Array with if statement to declare new index
---
+++ array-insert-shift/test.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main {
+  public static int[] addX(int n, int arr[],int x)
+    {
+        
+        double middle = arr.length % 2 == 0 ?  middle = arr.length / 2 : arr.length / 2 + 1 ;
+     
+        int newArr[] = new int[n+1];
+     for (int i = 0; i < n+1; i++){
+        if(i < middle) {
+         newarr[i] = arr[i];
+        }
+        if(i == middle) {
+         newArr[i] = x;
+        }
+        else if(i > middle) {
+         newArr[i] = arr[i -1];
+        }
+      }
+        return newArr;
+    }
+    public static void main(String[] args) {
+       // write your code here
+      int n = 10;
+      
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+ 
+        int x = 20;
+        arr = addX(n, arr,x);
+        System.out.println("\nArray with " + x
+                           + " added:\n"
+                           + Arrays.toString(arr));
+    }
+}
